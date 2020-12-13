65075ac168f8cba737c1353cd1004642f3ed6e3f
Allangr-eab <allangabrielrvsantos@yahoo.com.br>
2020-12-13 17:43:29 -0300

    Altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console . Clear ();
+            Console . WriteLine ( " --- Última Parcela " );
+            DateTime  DataAtual  =  DateTime . Now ;
+            Console . Write ( " Qual o valor total do financiamento ................: " );
+            double  valorFinanciamento  =  Convert . ToDouble ( Console . ReadLine ());
+            Console . Write ( " Quantidade de parcelas (sem juros) .................: " );
+            int  quantidadeParcela  =  Convert . ToInt32 ( Console . ReadLine ());
+            double  valorParcela  =  valorFinanciamento  /  quantidadeParcela ;
+            DateTime  ultimaParcela  =  DataAtual . AddMonths ( quantidadeParcela );
+            Console . Write (" Última parcela será em { ultimaParcela . ToShortDateString ()}, no valor de { valorParcela }. " );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UltimaParcela
 [console: UltimaParcela] Receba o valor total a ser pago e a quantidade de parcelas mensais de um financiamento. Exiba a data e o valor da última parcela.
+
+**Exempl0**:(vamos considerar a data atual 13/12/2020).
+
+--- Última Parcela
+Qual o valor total do financiamento................:985,12
+Quantidade de parcelas (sem juros).................:8
+Última parcela será em 13/08/2021, no valor de 123,14.
